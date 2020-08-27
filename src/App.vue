e957097b71d1b4c5a93a5aaace6cd3909ed47088
Administrator <qq844303749@gmail.com>
2020-08-27 09:35:24 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,20 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import AxiosInstance from "./client/AxiosInstance";
-import {UserClient} from "@/client/UserClient";
+  import Vue from 'vue';
+  import AxiosInstance from "./client/AxiosInstance";
 
 
-export default Vue.extend({
-  name: 'App',
-  components: {
-  },
-  async mounted() {
-    if(localStorage.getItem('token') !== null){
-      AxiosInstance.defaults.headers.common['x-api-key'] = localStorage.getItem('token')
-      this.$store.commit('login',localStorage.getItem('token'))
-      await this.$store.dispatch("getUserObj")
-    }
-  },
+  export default Vue.extend({
+    name: 'App',
+    components: {},
+    async mounted() {
+      if (localStorage.getItem('token') !== null) {
+        AxiosInstance.defaults.headers.common['x-api-key'] = localStorage.getItem('token')
+        this.$store.commit('login', localStorage.getItem('token'))
+        await this.$store.dispatch("getUserObj")
+      }
+    },
   data: () => ({
     //
   }),
